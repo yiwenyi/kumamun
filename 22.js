ac7505717b414c3b78f65fd2bd1091709ab399cf
易易妹 <evenywy@icloud.com>
2016-05-22 14:27:55 +0800

    change background

--- 22.js
+++ 22.js
@@ -7,6 +7,48 @@ $(document).ready(function () {
     var nowIndex = 0;
     var intervalTimer;
     $('.slider li:gt(0)').hide()
+    //canvas背景
+    var canvasWidth,canvasHeight,sliderHeight,sliderWidth;
+    var canvas = $('#canvas').get(0);
+    var ctx = canvas.getContext('2d');
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+    console.log(canvasWidth)
+    console.log(canvasHeight)
+    canvas.height = canvasHeight;
+    canvas.width = canvasWidth;
+    //slider定位
+    sliderHeight  = $('#scrollPics').height();
+    sliderWidth = $('#scrollPics').width();
+    //console.log(sliderHeight)
+    //console.log(sliderWidth)
+    $('#scrollPics').css('top',canvasHeight*0.5-sliderHeight*0.5)
+    $('#scrollPics').css('left',canvasWidth*0.5-sliderWidth*0.5)
+    //产生随机圆
+    randomCircle();
+    function randomCircle(){
+        ctx.clearRect(0,0,canvasWidth,canvasHeight);
+        ctx.fillStyle = "rgb(222,222,222)"
+        ctx.fillRect(0,0,canvasWidth,canvasHeight);
+        for(var i=0;i<20;i++){
+            x = Math.random()*canvasWidth;
+            y = Math.random()*canvasHeight;
+            R = Math.random()*canvasWidth*0.1;
+            g = Math.floor(Math.random()*255);
+            b = Math.floor(Math.random()*255);
+            r = Math.floor(Math.random()*255);
+            //粉色系
+            // r = 255;
+            // g = Math.floor(Math.random()*100);
+            // b = Math.floor(Math.random()*300-100);
+            ctx.globalAlpha = 0.3;
+            ctx.fillStyle = "rgb("+r+","+g+","+b+")";
+            ctx.beginPath();
+            ctx.arc(x,y,R,0,Math.PI*2);
+            ctx.closePath();
+            ctx.fill();
+        }}
+    setInterval(randomCircle,2000);
 //点击圆点切换
     $('.num li').click(function () {
         var index = $(this).index()
@@ -28,7 +70,7 @@ $(document).ready(function () {
     //点击左边箭头的切换
     $('.arrow-left').click(function () {
         //console.log(nowIndex)
-        if(nowIndex == 0){
+        if (nowIndex == 0) {
             nowIndex = num;
         }
         $('.num li').eq(nowIndex - 1).addClass('on').siblings('li').removeClass('on')
@@ -37,7 +79,7 @@ $(document).ready(function () {
     })
     //向右切换的方法
     function moveRight() {
-        if(nowIndex == num){
+        if (nowIndex == num) {
             nowIndex = -1;
         }
         $('.num li').eq(nowIndex + 1).addClass('on').siblings('li').removeClass('on')
@@ -54,8 +96,33 @@ $(document).ready(function () {
     function autoPlay() {
         intervalTimer = setInterval(function () {
             moveRight();
-        },3000)
+        }, 3000)
     }
+
     autoPlay();
 
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
